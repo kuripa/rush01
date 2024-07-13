f7115643d092ac4f794c487c09633a03bf4a0a87
kuripa <n.fuhrmann@protonmail.com>
2024-07-13 16:26:12 +0200

    feat: put hints string into 2d array

--- ex00/main.c
+++ ex00/main.c
@@ -9,87 +9,90 @@
 /*   Updated: 2024/07/13 12:34:10 by nfuhrman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+//#include "dbg.h"
 #include <unistd.h>
-
 
 /*TODO: remove this and create a dynamic array*/
 #define ROW 4
 #define COLUMN 4
 
-void solve(char *board[ROW][COLUMN])
-{
-    (void)board;
+void solve(char *board[ROW][COLUMN]) { (void)board; }
+
+void ft_putchar(char c) { write(1, &c, 1); }
+
+void ft_putnbr(int nbr) {
+  if (nbr < 0) {
+    ft_putchar('-');
+    ft_putnbr(-nbr);
+  } else if (nbr > 9) {
+    ft_putnbr(nbr / 10);
+    ft_putnbr(nbr % 10);
+  } else {
+    ft_putchar(nbr + '0');
+  }
 }
 
-void ft_putchar(char c)
-{
-    write(1, &c, 1); 
+void print_board(char board[ROW][COLUMN]) {
+  int i = 0;
+  int j = 0;
+  //(void)board;
+  while (i < COLUMN) {
+    j = 0;
+    while (j < ROW) {
+      ft_putchar(board[i][j]);
+      ft_putchar(' ');
+      j++;
+    }
+
+    ft_putchar('\n');
+    i++;
+  }
 }
 
-void ft_putnbr(int nbr)
-{
-    if (nbr < 0)
-    {
-        ft_putchar('-');
-        ft_putnbr(-nbr);
-    }
-    else if (nbr > 9)
-    {
-        ft_putnbr(nbr / 10);
-        ft_putnbr(nbr % 10);
-    }
-    else
-    {
-        ft_putchar(nbr + '0');
-    }
-}
+void split_string(char *hints, char hints_arr[4][4]) {
+  int i = 0;
+  int j = 0;
+  if (hints) {
 
-void print_board(char board[ROW][COLUMN])
-{
-    int i = 0;
-    int j = 0;
-    //(void)board;
-    while(i < COLUMN)
-    {
+    while (i < ROW && *hints) {
         j = 0;
-        while (j < ROW)
-        {
-            ft_putchar(board[i][j]);
-            ft_putchar(' ');
-            j++;
+      while (j < COLUMN) {
+        if (*hints >= '0' && *hints <= '9') {
+          hints_arr[i][j] = *hints;
+          j++;
         }
-       
-        ft_putchar('\n');
-        i++;
+        hints++;
+      }
+      i++;
     }
+  }
 }
 
-int main(int argc, char* argv[])
-{	
-    (void)argc;
-    (void)argv;
-    
-    
-    //char *hints = "4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2";
-    char board[ROW][COLUMN];
+int main(int argc, char *argv[]) {
+  (void)argc;
+  (void)argv;
 
-    int i = 0;
-    int j = 0;
-    while(i < ROW)
-    {
-        j = 0;
-        while (j < COLUMN)
-        {
-            board[i][j] = '0';
-            j++;
-        }
-        
-        i++;
+  // TODO: check if the input is valid
+
+  char *hints = "4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2";
+  char hints_arr[ROW][COLUMN];
+  char board[ROW][COLUMN];
+
+  int i = 0;
+  int j = 0;
+  while (i < ROW) {
+    j = 0;
+    while (j < COLUMN) {
+      board[i][j] = '0';
+      j++;
     }
-    
-    print_board(board);
 
+    i++;
+  }
 
-    return 0;
+  split_string(hints, hints_arr);
+  print_board(board);
+  print_board(hints_arr);
+
+  return 0;
 }
